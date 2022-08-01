0bd6a331ef97b42d1e47014d0180dbc8312aae51
gazzasaur <30276587+gazzasaur@users.noreply.github.com>
2022-07-31 20:39:31 -0700

    Adding a cli module

---
+++ src/k8s_builder/cli.py
@@ -0,0 +1,111 @@
+import sys
+import logging
+import argparse
+from k8s_builder import __version__
+
+__author__ = "gazzasaur"
+__copyright__ = "gazzasaur"
+__license__ = "MIT"
+
+_logger = logging.getLogger(__name__)
+
+
+# ---- CLI ----
+# The functions defined in this section are wrappers around the main Python
+# API allowing them to be called directly from the terminal as a CLI
+# executable/script.
+
+
+def parse_args(args):
+    """Parse command line parameters
+
+    Args:
+      args (List[str]): command line parameters as list of strings
+          (for example  ``["--help"]``).
+
+    Returns:
+      :obj:`argparse.Namespace`: command line parameters namespace
+    """
+    parser = argparse.ArgumentParser(description="Just a Fibonacci demonstration")
+    parser.add_argument(
+        "--version",
+        action="version",
+        version="k8s_builder {ver}".format(ver=__version__),
+    )
+
+    parser.add_argument(
+        "-f",
+        "--file",
+        dest="config_file",
+        help="Read the cluster setup information from a file",
+        type=str,
+        metavar="CONFIGPATH",
+    )
+    parser.add_argument(
+        "-u",
+        "--url",
+        dest="config_url",
+        help="Read the cluster setup information from a url",
+        type=str,
+        metavar="CONFIGURL",
+    )
+
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        dest="loglevel",
+        help="set loglevel to INFO",
+        action="store_const",
+        const=logging.INFO,
+    )
+    parser.add_argument(
+        "-vv",
+        "--very-verbose",
+        dest="loglevel",
+        help="set loglevel to DEBUG",
+        action="store_const",
+        const=logging.DEBUG,
+    )
+    return parser.parse_args(args)
+
+
+def setup_logging(loglevel):
+    """Setup basic logging
+
+    Args:
+      loglevel (int): minimum loglevel for emitting messages
+    """
+    logformat = "[%(asctime)s] %(levelname)s:%(name)s:%(message)s"
+    logging.basicConfig(
+        level=loglevel, stream=sys.stdout, format=logformat, datefmt="%Y-%m-%d %H:%M:%S"
+    )
+
+
+def main(args):
+    """Wrapper allowing :func:`fib` to be called with string arguments in a CLI fashion
+
+    Instead of returning the value from :func:`fib`, it prints the result to the
+    ``stdout`` in a nicely formatted message.
+
+    Args:
+      args (List[str]): command line parameters as list of strings
+          (for example  ``["--verbose", "42"]``).
+    """
+    args = parse_args(args)
+    setup_logging(args.loglevel)
+    _logger.debug("Beginning cluster setup...")
+
+    _logger.info("Finished cluster setup")
+
+
+def run():
+    """Calls :func:`main` passing the CLI arguments extracted from :obj:`sys.argv`
+
+    This function can be used as entry point to create console scripts with setuptools.
+    """
+    main(sys.argv[1:])
+
+
+if __name__ == "__main__":
+    #     python -m k8s_builder.cli 42
+    run()
